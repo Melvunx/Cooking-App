c169c1ef14cef76490d82497781222136ba40901
Melvunx <augustine.melv@gmmail.com>
2024-10-10 23:34:41 +0200

    setup search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ const Header = () => {
   return (
     <header className="flex h-screen flex-col items-center justify-center gap-24 bg-slate-700">
       <div className="flex flex-col items-center justify-center rounded-lg bg-zinc-200 ">
-        <h1 className="p-20 font-title text-6xl text-zinc-700 transition-colors hover:text-amber-600">
+        <h1 className="cursor-default p-20 font-title text-6xl text-zinc-700 transition-colors hover:text-amber-600">
           Welcome to CheckMeals !
         </h1>
       </div>
-      <a href="#main">
-        <button className="flex items-center gap-5 rounded-md border bg-neutral-700 p-3 font-global text-lg font-bold text-white shadow-lg ring-slate-900 transition-transform hover:scale-110 hover:bg-neutral-500/80 active:scale-75 max-md:w-1/3 max-md:justify-evenly max-md:gap-0">
+      <a href="#main" className=" flex w-1/3 justify-center">
+        <button className="flex items-center gap-5 rounded-md border bg-neutral-700 p-3 font-global text-lg font-bold text-white shadow-lg ring-slate-900 transition-transform hover:scale-110 hover:bg-neutral-500/80 active:scale-75  max-md:w-full max-md:justify-evenly max-md:gap-0 max-md:text-center">
           <span className="animate-pulse">
             <ArrowBigDown />
           </span>{" "}
