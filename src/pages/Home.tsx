a80ef6aecd5d678a297d4fe7ba84173459d264ae
Melvunx <augustine.melv@gmmail.com>
2024-10-10 19:03:48 +0200

    setup project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Card from "../components/layout/Card";
+import Search from "../components/layout/Search";
+
+const Home = () => {
+  return (
+    <div>
+      <Search />
+      <main>
+        <Card />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
