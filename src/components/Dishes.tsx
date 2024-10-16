a357c46334d68b1729f41495266e1b1b13dbb529
Melvunx <augustine.melv@gmmail.com>
2024-10-16 21:20:10 +0200

    addons input search

--- src/components/Dishes.tsx
+++ src/components/Dishes.tsx
@@ -10,7 +10,7 @@ const Dishes = () => {
   useEffect(() => {
     axios
       .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchTerm)
-      //S'il n'y a pas de résultat possible, on envoie un tableau vide !
+      // S'il n'y a pas de résultat possible, on envoie un tableau vide
       .then((res) => setData(res.data.meals || []));
   }, [searchTerm]);
 
@@ -26,18 +26,30 @@ const Dishes = () => {
             Tap your research
           </h3>
           <input
-            className="rounded-sm border p-2 shadow-sm outline-none ring-2 transition-colors placeholder:font-global placeholder:italic placeholder:tracking-wide hover:bg-blue-50 hover:placeholder:text-zinc-500 hover:placeholder:transition-colors focus:bg-blue-100 focus:placeholder:text-zinc-800/60"
+            className={`rounded-sm border p-2 font-global shadow-sm outline-none  transition-colors 
+              ${
+                data.length > 0
+                  ? "border-green-500 text-green-500 ring-2"
+                  : searchTerm && data.length === 0
+                  ? "border-red-500 text-red-500"
+                  : ""
+              }
+              placeholder:font-global placeholder:italic placeholder:tracking-wide hover:bg-blue-50 
+              hover:placeholder:text-zinc-500 hover:placeholder:transition-colors focus:bg-blue-100 
+              focus:placeholder:text-zinc-800/60`}
             type="text"
-            placeholder="ex: beef..."
+            placeholder="ex: beef"
             onInput={(e) => setSearchTerm((e.target as HTMLInputElement).value)}
             onFocus={() => setIsFocus(true)}
             onBlur={() => setIsFocus(false)}
           />
         </div>
       </div>
-      <ul className="flex flex-wrap gap-8 max-md:mx-auto max-md:w-10/12 lg:w-full">
+      <ul className="flex flex-wrap justify-center gap-8 max-md:mx-auto max-md:w-10/12 lg:w-full">
         {data.length === 0 ? (
-          <h1>No results found</h1>
+          <h1 className="cursor-default py-8 text-center font-global text-3xl tracking-wide text-zinc-500 transition-transform hover:scale-110 hover:text-red-800">
+            No results found
+          </h1>
         ) : (
           data
             .slice(0, 16)
--- src/components/layout/Card.tsx
+++ src/components/layout/Card.tsx
@@ -23,13 +23,15 @@ const Card = ({ meal }: { meal: Meal }) => {
           className="transition-colors hover:animate-pulse hover:text-red-600"
           href={meal.strYoutube}
         >
-          <TvMinimalPlay size={30} strokeWidth={1} />
+          <TvMinimalPlay size={32} strokeWidth={1} />
         </a>
       </div>
-      <h1>
+      <h1 className="cursor-default font-title text-xl tracking-wider text-lime-900 transition-colors hover:text-lime-600">
         {meal.strMeal} - {meal.strArea}
       </h1>
-      <p>Category : {meal.strCategory}</p>
+      <p className="cursor-default text-lg italic tracking-wide text-gray-600 transition-transform hover:scale-110">
+        Category : {meal.strCategory}
+      </p>
       <button className="rounded-sm border border-slate-900 bg-slate-800 px-5 py-2 font-bold text-slate-200 shadow-lg transition-colors hover:bg-slate-600">
         <a href={meal.strSource} className="">
           Read the recipe
